c14338f4f457a07b9b16e3d2246e24019aa30505
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2018-06-02 12:35:17 +0200

    Last modifications

--- qproteo.bat
+++ qproteo.bat
@@ -1,15 +1,18 @@
 @echo off
 
 :: get current path
-SET PWD=%~dp0
-SET PWD=%PWD:~0,-1%
+REM SET PWD=%~dp0
+REM SET PWD=%PWD:~0,-1%
 
 :: environment variables
-SET QPROTEO=%PWD%
-SET QPROTEO_VENV=%QPROTEO%/.venv_win
-SET QPROTEO_VENV_ACTIVE=%QPROTEO_VENV%/Scripts/activate.bat
-SET WF_PATH=%QPROTEO%
-SET TESTS_PATH=%QPROTEO%/test
+REM SET QPROTEO=%PWD%
+REM SET VENV=%QPROTEO%/venv_win64/venv_win64_py3x
+
+:: environment variables
+SET VENV=%QPROTEO_HOME%/venv_win64/venv_win64_py3x
+SET VENV_ACTIVE=%VENV%/Scripts/activate.bat
+SET WF_PATH=%QPROTEO_HOME%
+SET TESTS_PATH=%QPROTEO_HOME%/test
 
 :: workflow variables
 SET WF_SMK_FILE=%WF_PATH%/qproteo.smk
@@ -22,6 +25,6 @@ REM SET WF_CONF_FILE=%TESTS_PATH%/test2-conf.tmt.yml
 SET /p WF_CONF_FILE="Enter the input file for the config workflow (in YAML extension): "
 
 :: execute workflow
-CMD /k "%QPROTEO_VENV_ACTIVE% && snakemake.exe --configfile %WF_CONF_FILE% --snakefile %WF_SMK_FILE% --unlock && snakemake.exe --configfile %WF_CONF_FILE% --snakefile %WF_SMK_FILE% -j %WF_NTHREADS% --rerun-incomplete "
+CMD /k "%VENV_ACTIVE% && snakemake.exe --configfile %WF_CONF_FILE% --snakefile %WF_SMK_FILE% --unlock && snakemake.exe --configfile %WF_CONF_FILE% --snakefile %WF_SMK_FILE% -j %WF_NTHREADS% --rerun-incomplete "
 
 REM SET /P DUMMY=Hit ENTER to continue...
