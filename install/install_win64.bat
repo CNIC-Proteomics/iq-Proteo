e15e4780b2805d81961404969e373f4f8f70deab
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2018-12-19 16:38:11 +0100

    Fixing the installation

--- install/install_win64.bat
+++ install/install_win64.bat
@@ -6,32 +6,30 @@ ECHO **
 ECHO ** sets the env. variables from input parameters:
 
 ECHO **
-REM  SET  pwd=%~dp0
-REM  SET  pwd=%pwd:~0,-1%
 FOR  %%i in ("%~dp0..") do SET "SRC_HOME=%%~fi"
 SET  SRC_HOME=%SRC_HOME:"=%
 
 ECHO **
 SET  LIB_VERSION=0.1
-REM SET  LIB_PATH=""
-REM SET  /p LIB_PATH="** Enter the path where iq-Proteo libraries will be saved: "
-SET  LIB_PATH="D:\iq Proteo\library"
+SET  LIB_PATH=""
+SET  /p LIB_PATH="** Enter the path where iq-Proteo libraries will be saved: "
+REM SET  LIB_PATH="D:\iq Proteo\library"
 IF   %LIB_PATH% =="" GOTO :EndProcess1
 SET  LIB_PATH=%LIB_PATH:"=%
 SET  LIB_HOME=%LIB_PATH%/%LIB_VERSION%
 SET  PYENV_HOME=%LIB_HOME%/python_venv
 
 ECHO **
-REM SET  PYTHON3x_HOME=""
-REM SET  /p PYTHON3x_HOME="** Enter the path to Python 3 (3.6.7 version!!): "
-SET  PYTHON3x_HOME="D:\softwares\Python3.6.7"
+SET  PYTHON3x_HOME=""
+SET  /p PYTHON3x_HOME="** Enter the path to Python 3 (3.6.7 version!!): "
+REM SET  PYTHON3x_HOME="D:\softwares\Python3.6.7"
 IF   %PYTHON3x_HOME% =="" GOTO :EndProcess2
 SET  PYTHON3x_HOME=%PYTHON3x_HOME:"=%
 
 ECHO **
-REM SET  R_HOME=""
-REM SET  /p R_HOME="** Enter the path to R: "
-SET  R_HOME="C:\Program Files\R\R-3.5.1"
+SET  R_HOME=""
+SET  /p R_HOME="** Enter the path to R: "
+REM SET  R_HOME="C:\Program Files\R\R-3.5.1"
 IF   %R_HOME% =="" GOTO :EndProcess3
 SET  R_HOME=%R_HOME:"=%
 SET  R_LIB=%LIB_HOME%/R
