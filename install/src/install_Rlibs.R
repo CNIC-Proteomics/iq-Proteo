a399b164142feee695d45b8b8182f68bc4af2e73
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2018-12-19 16:53:03 +0100

    Fixing the installation

--- install/src/install_Rlibs.R
+++ install/src/install_Rlibs.R
@@ -10,7 +10,7 @@ lib_path <- args[1]
 # create library directory
 dir.create(lib_path, showWarnings = FALSE, recursive = TRUE)
 
-install the list of packages
+# install the list of packages
 list.of.packages <- c("RSQLite", "optparse", "readr", "stringi", "plyr", "Peptides", "XML")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos="http://cran.us.r-project.org", lib=lib_path)
