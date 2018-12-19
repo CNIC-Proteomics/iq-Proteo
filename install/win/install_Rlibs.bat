22f6dfcfcb3e7dbd862537413ea9b7db3abb0cfc
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2018-12-19 16:38:52 +0100

    Fixing the installation

--- install/win/install_Rlibs.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-:: install R packages ----------------------
-ECHO **
-ECHO **
-ECHO ** install R packages
-REM CMD /C " "%IQPROTEO_R_HOME%/bin/R" --vanilla < "%IQPROTEO_SRC_HOME%/install/src/install_Rlibs.R" "
-ECHO " "%IQPROTEO_R_HOME%/bin/R" --vanilla < "%IQPROTEO_SRC_HOME%/install/src/install_Rlibs.R" "
-
